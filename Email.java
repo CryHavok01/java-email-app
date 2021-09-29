bb83e6929539b838b2fe7149c3c6b4d3750469ec
Josh Halpern <jhhalpern@gmail.com>
2021-09-29 16:45:19 -0400

    password generator

--- Email.java
+++ Email.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Email {
   private String firstName;
   private String lastName;
   private String password;
+  private String department;
   private int mailboxCapacity;
   private String alternateEmail;
 
@@ -10,6 +13,42 @@ public class Email {
     this.lastName = lastName;
     System.out.println("Email created for " + this.firstName + " " + this.lastName);
 
+    this.department = setDepartment();
+    System.out.println("Department set as: " + this.department);
+    
+    this.password = generatePassword(8);
+    System.out.println("Password set as: " + this.password);
+
+  }
+
+  private String setDepartment() {
+    System.out.println("Enter the department:");
+    System.out.println("1 - Sales");
+    System.out.println("2 - Development");
+    System.out.println("3 - Accounting");
+    System.out.println("0 - None");
+
+    Scanner in = new Scanner(System.in);
+    int depChoice = in.nextInt();
+    if (depChoice == 1) {
+      return "sales";
+    } else if (depChoice == 2) {
+      return "dev";
+    } else if (depChoice == 3) {
+      return "acct";
+    } else {
+      return "";
+    }
+  }
+
+  private String generatePassword(int length) {
+    String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+    char[] password = new char[length];
+    for (int i = 0; i < length; i++) {
+      int random = (int) (Math.random() * characters.length());
+      password[i] = characters.charAt(random);
+    }
+    return new String(password);
   }
   public static void main(String[] args) {
     
