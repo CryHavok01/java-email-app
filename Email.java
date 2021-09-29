5af9fc73e93ecbce0cde36b73a6b274d1ab9083b
Josh Halpern <jhhalpern@gmail.com>
2021-09-29 16:23:24 -0400

    initial commit

---
+++ Email.java
@@ -0,0 +1,7 @@
+public class Email {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
---
+++ Email_App.java
@@ -0,0 +1,7 @@
+public class Email_App {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
